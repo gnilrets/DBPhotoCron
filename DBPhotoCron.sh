91c8be4dc15f513366c2fc2c085d037d6194271b
Sterling Paramore <sterling.paramore@greatvines.com>
2013-04-26 21:29:00 -0700

    Syncs last two months to a shared folder

--- DBPhotoCron.sh
+++ DBPhotoCron.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-#Not going to pay $14 for a nice app when I can just write a simple crontab
+###   Description
 
+# Add this job to crontab, run as frequently as you like
+
+# Inspired by Hazel, but not going to pay $14 for a 
+# nice app when I can just write a simple crontab
+
+# Assumes all photos are organized into folders YYYY/YYYY-MM in ARCHDIR
+
+###   Config 
 # This is where dropbox puts camera uploads
 SRCDIR="/Users/gnilrets/Dropbox/Camera Uploads"
 
-#This is the root of where I want the files to live
-OUTDIR="/Users/gnilrets/Dropbox/Photos-Master"
+# This is the root of where I want the files to live
+ARCHDIR="/Users/gnilrets/Dropbox/Photos-Master"
+
+# This is where I want to put copies for sharing
+SHAREDIR="/Users/gnilrets/Dropbox/Shared/ByMe/ParamoreFamilyPhotos"
+
+
+###   Archive camera uploads
+
+# Fortunately, the dropbox camera uploader make the create date of the file
+# equal to the time the photo was taken.  So archiving them to the appropriate folder
+# is easy.
 
 # Move files from the dropbox camera folder into the target/YYYY/YYYY-MM folders
 find "$SRCDIR" -type f | while read file; do
@@ -14,7 +32,27 @@ find "$SRCDIR" -type f | while read file; do
     YEAR=`stat -f "%Sm" -t "%Y" "$file"`
     YRMO=`stat -f "%Sm" -t "%Y-%m" "$file"`
     echo $YEAR/$YRMO
-    mkdir -p "$OUTDIR/$YEAR/$YRMO"
-    mv "$file" "$OUTDIR/$YEAR/$YRMO"
+    mkdir -p "$ARCHDIR/$YEAR/$YRMO"
+    mv "$file" "$ARCHDIR/$YEAR/$YRMO"
 done
 
+### Push recent photos to my shared folder
+#  warning: this will push any pictures synced to dropbox to a public folder.
+#  BE WARNED
+
+# Let's do something really simple - share the last two months of files
+
+CURRENTYR=$(date +"%Y")
+CURRENTMO=$(date +"%Y/%Y-%m")
+LASTYR=$(date -v-1m +"%Y")
+LASTMO=$(date -v-1m +"%Y/%Y-%m")
+echo CURRENTYR=$CURRENTYR LASTYR=$LASTYR
+echo CURRENTMO=$CURRENTMO LASTMO=$LASTMO
+
+rsync -avz --delete-excluded \
+    --include="$CURRENTYR" --include="$CURRENTMO" --include="/$CURRENTMO/**" \
+    --include="$LASTYR" --include="$LASTMO" --include="/$LASTMO/**" \
+    --exclude=* \
+    $ARCHDIR/ $SHAREDIR/
+
+# TODO: Favorites only
